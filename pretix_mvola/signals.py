99d14ef187647b9c3a9901bea541fce70a61d04d
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2024-11-18 13:10:47 +0100

    feat: add global settings key

--- pretix_mvola/signals.py
+++ pretix_mvola/signals.py
@@ -1,1 +1,39 @@
-# Register your receivers here
+from collections import OrderedDict
+from django import forms
+from django.dispatch import receiver
+from django.utils.translation import gettext_lazy as _
+from pretix.base.forms import SecretKeySettingsField
+from pretix.base.signals import register_global_settings, register_payment_providers
+
+
+@receiver(register_global_settings, dispatch_uid="payment_mvola_global_settings")
+def register_global_settings(sender, **kwargs):
+    return OrderedDict(
+        [
+            (
+                "payment_mvola_consumer_key",
+                forms.CharField(
+                    label=_("Mvola: Consumer key"),
+                    required=True,
+                ),
+            ),
+            (
+                "payment_mvola_secret_key",
+                SecretKeySettingsField(
+                    label=_("Mvola: Secret Key"),
+                    required=True,
+                ),
+            ),
+            (
+                "payment_mvola_status",
+                forms.ChoiceField(
+                    label=_("Mvola: Status"),
+                    initial="live",
+                    choices=(
+                        ("SANDBOX", "SANDBOX"),
+                        ("PRODUCTION", "PRODUCTION"),
+                    ),
+                ),
+            ),
+        ]
+    )
